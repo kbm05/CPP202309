6795ca4f192411efb857b1728a178f0ffb2263a7
kbm05 <ksyqp5@naver.com>
2023-09-13 20:48:25 +0900

    practice/week2/test.cpp

---
+++ practice/week2/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 10;
+	int b = 15;
+	cout << a + b << endl;
+	
+	char text1[10] = "Hello";
+	char text2[50] = "World";
+	//char 타입의 배열 생성, 괄호속 숫자는 배열의 크기
+
+	cout << text1 << "!" << endl;
+	cout << text2 << "!" << endl;
+
+	return 0;
+}
